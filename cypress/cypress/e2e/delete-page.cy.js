d6bbb87d2b7a3baf8d89d121744b536108a41e66
dparejaUniandes <d.pareja@uniandes.edu.co>
2023-11-12 19:50:23 -0500

    Finished the page scenarios

--- cypress/cypress/e2e/delete-page.cy.js
+++ cypress/cypress/e2e/delete-page.cy.js
@@ -1,4 +1,7 @@
-describe('Create a page', () => {
+import { faker } from '@faker-js/faker';
+
+// Funcionalidad 7 Eliminar "Pages"
+describe('Delete a page', () => {
     // Given
     beforeEach(()=>{
       cy.login();
@@ -6,12 +9,32 @@ describe('Create a page', () => {
       cy.get('a[href="#/editor/page/"]').click();
     })
    
+    // Escenario 7.1
     it('Eliminar una página', () => {
         // When
-        cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').type('Draft page');
-        cy.get('p[data-koenig-dnd-droppable="true"]').type('Draft page description');
-        cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click()
+        const pageTitle = faker.lorem.sentence();
+        const pageContent = faker.lorem.paragraphs(1);
+        cy.get('textarea.gh-editor-title.ember-text-area').type(pageTitle);
+        cy.get('p[data-koenig-dnd-droppable="true"]').type(pageContent);
+        cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
         cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
         cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').click();
+        // The before cy.get is to confirm button delete and generate a console error avoiding the next verification
+        cy.get('gh-content-entry-title').contains(pageTitle).should('not.exist'); 
+    })
+
+    // Escenario 7.2
+    it('Abortar eliminación de una página', () => {
+        // When
+        const pageTitle = faker.lorem.sentence();
+        const pageContent = faker.lorem.paragraphs(1);
+        cy.get('textarea.gh-editor-title.ember-text-area').type(pageTitle);
+        cy.get('p[data-koenig-dnd-droppable="true"]').type(pageContent);
+        cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
+        cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
+        cy.get('.modal-footer button.gh-btn').contains('Cancel').click();
+        cy.get('a[href="#/pages/"]').click();
+        cy.wait(1000)
+        cy.get('gh-content-entry-title').contains(pageTitle).should('exist'); 
     })
   })
