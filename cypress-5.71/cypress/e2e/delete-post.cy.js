ba6c5afc0a6470816d797440ea4432e227dc2e83
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2023-11-18 21:02:43 -0500

    Add screenshot for cypress 5.71

--- cypress-5.71/cypress/e2e/delete-post.cy.js
+++ cypress-5.71/cypress/e2e/delete-post.cy.js
@@ -2,46 +2,63 @@ import { faker } from '@faker-js/faker';
 
 // Funcionalidad 3 Eliminar Posts
 describe('Delete a Post', () => {
-    // Given
-    beforeEach(()=>{
-      cy.login();
-      cy.wait(2000);
-      cy.get('a[href="#/posts/"]').click();
-      cy.get('a[href="#/editor/post/"]').then((btns) =>{
+  let initScreenshot = false;
+
+  // Given
+  beforeEach(()=>{
+    cy.login(initScreenshot ? false : '3.delete-post');
+    cy.wait(2000);
+    if(!initScreenshot){
+      cy.screenshot('3.1-0');
+      initScreenshot = true;
+    }
+    cy.get('a[href="#/posts/"]').click();
+    cy.get('a[href="#/editor/post/"]').then((btns) =>{
+      const index = Math.floor(Math.random() * btns.length);
+      btns[index].click();
+    });
+  })
+  
+  // Escenario 3.1
+  it('3.1 Eliminar un post', () => {
+    cy.screenshot('3.1-1'); // llegar a la pagina
+    // When
+    const postTitle = faker.lorem.sentence();
+    const postContent = faker.lorem.paragraphs(1);
+    cy.get('textarea.gh-editor-title.ember-text-area').type(postTitle);
+    cy.get('p[data-koenig-dnd-droppable="true"]').type(postContent);
+    cy.screenshot('3.1-2');
+    cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
+    cy.screenshot('3.1-3');
+    cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
+    cy.screenshot('3.1-4');
+    cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').click();
+    cy.screenshot('3.1-5');
+    // The before cy.get is to confirm button delete and generate a console error avoiding the next verification
+    cy.get('gh-content-entry-title').contains(postTitle).should('not.exist'); 
+  })
+
+  // Escenario 3.2
+  it('3.2 Abortar eliminación de un post', () => {
+    cy.screenshot('3.2-1'); // llegar a la pagina
+    // When
+    const postTitle = faker.lorem.sentence();
+    const postContent = faker.lorem.paragraphs(1);
+    cy.get('textarea.gh-editor-title.ember-text-area').type(postTitle);
+    cy.get('p[data-koenig-dnd-droppable="true"]').type(postContent);
+    cy.screenshot('3.2-2');
+    cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
+    cy.screenshot('3.2-3');
+    cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
+    cy.screenshot('3.2-4');
+    cy.get('.modal-footer button.gh-btn').contains('Cancel').click();
+    cy.screenshot('3.2-5');
+    cy.get('a[href="#/posts/"]').then((btns) =>{
         const index = Math.floor(Math.random() * btns.length);
         btns[index].click();
       });
-    })
-   
-    // Escenario 3.1
-    it('3.1 Eliminar un post', () => {
-        // When
-        const postTitle = faker.lorem.sentence();
-        const postContent = faker.lorem.paragraphs(1);
-        cy.get('textarea.gh-editor-title.ember-text-area').type(postTitle);
-        cy.get('p[data-koenig-dnd-droppable="true"]').type(postContent);
-        cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
-        cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
-        cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').click();
-        // The before cy.get is to confirm button delete and generate a console error avoiding the next verification
-        cy.get('gh-content-entry-title').contains(postTitle).should('not.exist'); 
-    })
-
-    // Escenario 3.2
-    it('3.2 Abortar eliminación de un post', () => {
-        // When
-        const postTitle = faker.lorem.sentence();
-        const postContent = faker.lorem.paragraphs(1);
-        cy.get('textarea.gh-editor-title.ember-text-area').type(postTitle);
-        cy.get('p[data-koenig-dnd-droppable="true"]').type(postContent);
-        cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor').click()
-        cy.get('button.gh-btn.gh-btn-outline.gh-btn-icon.gh-btn-fullwidth').click();
-        cy.get('.modal-footer button.gh-btn').contains('Cancel').click();
-        cy.get('a[href="#/posts/"]').then((btns) =>{
-            const index = Math.floor(Math.random() * btns.length);
-            btns[index].click();
-          });
-        cy.wait(1000)
-        cy.get('a.gh-post-list-title').contains(postTitle); 
-    })
+    cy.wait(1000)
+    cy.screenshot('3.2-6');
+    cy.get('a.gh-post-list-title').contains(postTitle); 
   })
+})
--- cypress-5.71/cypress/e2e/edit-post.cy.js
+++ cypress-5.71/cypress/e2e/edit-post.cy.js
@@ -2,11 +2,20 @@ import { faker } from '@faker-js/faker';
 
 describe('Edit a post', () => {
   const postTitleNewPost = faker.lorem.sentence();
+  let initScreenshot = false;
+
   beforeEach(()=>{
     // Given
-    cy.login();
+    cy.login(initScreenshot ? false : '2.edit-post' );
     cy.wait(2000);
-    cy.get('a[href="#/posts/"]').click();
+    if(!initScreenshot){
+      cy.screenshot('2-0');
+      initScreenshot = true;
+    }
+    cy.get('a[href="#/posts/"]').then((btns) =>{
+      const index = Math.floor(Math.random() * btns.length);
+      btns[index].click();
+    });
     cy.get('a[href="#/editor/post/"]').then((btns) =>{
         const index = Math.floor(Math.random() * btns.length);
         btns[index].click();
@@ -26,51 +35,60 @@ describe('Edit a post', () => {
   })
 
   it('2.1 Editar un post y ver el cambio en la vista de usuario final', () => {
+    cy.screenshot('2.1-1');
     // When
     const postTitleToEdit = faker.lorem.sentence();
     cy.get('.gh-post-list-title').first().click()
     cy.wait(1000)
     cy.get('textarea[placeholder="Post title"]').clear().type(postTitleToEdit);
     cy.wait(2000)
+    cy.screenshot('2.1-2');
     cy.get('a[href="#/posts/?type=draft"]').then((btns) =>{
         const index = Math.floor(Math.random() * btns.length);
         btns[index].click();
       });
     cy.wait(1000)
 
+    cy.screenshot('2.1-3');
     // Then
     cy.get('.gh-post-list-title').first().find('.gh-content-entry-title').should('contain', postTitleToEdit);
   })
 
   it('2.2 Editar un post, dejarlo vacío e intentar guardarlo', () => {
+    cy.screenshot('2.2-1');
     // When
     cy.get('.gh-post-list-title').first().click()
     cy.wait(1000)
     cy.get('textarea[placeholder="Post title"]').clear();
     cy.wait(2000)
+    cy.screenshot('2.2-2');
     cy.get('a[href="#/posts/?type=draft"]').then((btns) =>{
         const index = Math.floor(Math.random() * btns.length);
         btns[index].click();
       });
     cy.wait(1000)
 
+    cy.screenshot('2.2-3');
     // Then
     cy.get('.gh-post-list-title').first().find('.gh-content-entry-title').should('contain', "");
   })
 
   it('2.3 Ingresar a editar un post, modificar algún campo pero no guardar', () => {
+    cy.screenshot('2.3-1');
     // When
     const postTitleToEdit = faker.lorem.sentence();
     cy.get('.gh-post-list-title').first().click()
     cy.wait(1000)
     cy.get('textarea[placeholder="Post title"]').clear().type(postTitleToEdit);
     cy.wait(2000)
+    cy.screenshot('2.3-2');
     cy.get('a[href="#/posts/?type=draft"]').then((btns) =>{
         const index = Math.floor(Math.random() * btns.length);
         btns[index].click();
       });
     cy.wait(1000)
 
+    cy.screenshot('2.3-3');
     // Then, expected to fail because update the title and it shouldn't
     cy.get('.gh-post-list-title').first().find('.gh-content-entry-title').should('contain', postTitleNewPost);
   })
